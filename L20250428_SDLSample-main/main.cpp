42090c12840bf55f2d62ee70c756b362353c43f4
dhxogud <hyong0117@naver.com>
2025-04-29 13:45:45 +0900

    강사님코드로 수정

--- L20250428_SDLSample-main/main.cpp
+++ L20250428_SDLSample-main/main.cpp
@@ -1,88 +1,98 @@
-#include <vector>
 #include "SDL3/SDL.h"
 
 #pragma comment(lib, "SDL3")
-void DrawCircle(SDL_Renderer* renderer, int centerX, int centerY, int radius) {
-	int x = radius;
-	int y = 0;
-	int err = 0;
-
-	while (x >= y) {
-		SDL_RenderPoint(renderer, centerX + x, centerY + y);
-		SDL_RenderPoint(renderer, centerX + y, centerY + x);
-		SDL_RenderPoint(renderer, centerX - y, centerY + x);
-		SDL_RenderPoint(renderer, centerX - x, centerY + y);
-		SDL_RenderPoint(renderer, centerX - x, centerY - y);
-		SDL_RenderPoint(renderer, centerX - y, centerY - x);
-		SDL_RenderPoint(renderer, centerX + y, centerY - x);
-		SDL_RenderPoint(renderer, centerX + x, centerY - y);
-
-		if (err <= 0) {
-			y += 1;
-			err += 2 * y + 1;
-		}
-		else {
-			x -= 1;
-			err -= 2 * x + 1;
-		}
-	}
-}
 
 int main()
 {
-	// Initialize
 	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
 
 	SDL_Window* MyWindow = SDL_CreateWindow("Sample", 640, 480, SDL_WINDOW_OPENGL);
 
-	SDL_Renderer* MyRenderer = SDL_CreateRenderer(MyWindow, NULL);
+	SDL_Renderer* MyRenderer = SDL_CreateRenderer(MyWindow, nullptr);
 
-	// 프로그램끼리 통신할때 "물어보는 행위"를 Polling 폴링 한다고 한다.
-	bool isRunning = true;
+	bool IsRunning = true;
 	SDL_Event MyEvent;
-	while (isRunning)
+
+	int NumKeys = 0;
+	const bool* KeyStates = SDL_GetKeyboardState(&NumKeys);
+
+	int PlayerX = 0;
+	int PlayerY = 0;
+	while (IsRunning)
 	{
 		SDL_PollEvent(&MyEvent);
 		switch (MyEvent.type)
 		{
 		case SDL_EVENT_QUIT:
-			isRunning = false;
+			IsRunning = false;
 			break;
+		case SDL_EVENT_KEY_DOWN:
+			if (KeyStates[SDL_SCANCODE_UP])
+			{
+				PlayerY -= 10;
+			}
+			if (KeyStates[SDL_SCANCODE_DOWN])
+			{
+				PlayerY += 10;
+			}
+			if (KeyStates[SDL_SCANCODE_LEFT])
+			{
+				PlayerX -= 10;
+			}
+			if (KeyStates[SDL_SCANCODE_RIGHT])
+			{
+				PlayerX += 10;
+			}
+			if (KeyStates[SDL_SCANCODE_ESCAPE])
+			{
+				IsRunning = false;
+			}
 		default:
 			break;
 		}
 
-
-		// SDL_Renderer 의 색상 정하기
 		SDL_SetRenderDrawColor(MyRenderer, 0, 0, 0, 0);
-
-		// 전체 화면에 칠하기
 		SDL_RenderClear(MyRenderer);
 
-
-
-		// 사각형
-		SDL_FRect Rectangle[100] = { NULL, };;
-		for (int i = 0; i < 100; i++)
-		{
-			SDL_FRect Rect = { SDL_rand(640), SDL_rand(480), 10, 10 };
-			Rectangle[i] = Rect;
-		}
-		SDL_SetRenderDrawColor(MyRenderer, 255, 0, 0, 0);
-		SDL_RenderFillRects(MyRenderer, Rectangle, 100);
-
-		// 원
+		SDL_FRect Player = { (float) PlayerX, (float) PlayerY, 100, 100 };
 		SDL_SetRenderDrawColor(MyRenderer, 255, 255, 255, 0);
-		DrawCircle(MyRenderer, 320, 240, 100);
-		SDL_RenderPresent(MyRenderer);
+		SDL_RenderFillRect(MyRenderer, &Player);
 
-		//CPU가 RAM에 보낼 값들. 앞으로 GPU가 출력할때 필요한 정보값들을 stack형식마냥 쌓아두고
-		// SDL_RenderFillRects(MyRenderer, ...);
-		// RAM의 stack 에 쌓여 있는 Rect들을 한번에 GPU로 보내 출력함
+		//for (int i = 0; i < 1000; ++i)
+		//{
+		//	SDL_SetRenderDrawColor(MyRenderer,
+		//		SDL_rand(255),
+		//		SDL_rand(255),
+		//		SDL_rand(255),
+		//		0);
+		//	SDL_FRect MyRect{ SDL_randf() * 640 - 320,
+		//		SDL_randf() * 480 - 240,
+		//		SDL_randf() * 640, SDL_randf() * 480
+		//	};
+		//	SDL_RenderFillRect(MyRenderer, &MyRect);
+		//}
+
+		//SDL_SetRenderDrawColor(MyRenderer, 255, 0, 0, 0);
+
+		//int Radius = 200;
+		//int X = 320;
+		//int Y = 240;
+		//int PrevCirCleX = Radius * SDL_cos(0) + X;
+		//int PrevCirCleY = Radius * SDL_sin(0) + Y;
+		//for (int Angle = 10; Angle <= 360; Angle += 10 )
+		//{
+		//	int CircleX = Radius * SDL_cos(Angle * SDL_PI_F / 180) + X;
+		//	int CircleY = Radius * SDL_sin(Angle * SDL_PI_F / 180) + Y;
+		//	SDL_RenderLine(MyRenderer, PrevCirCleX, PrevCirCleY,
+		//		CircleX, CircleY);
+		//	PrevCirCleX = CircleX;
+		//	PrevCirCleY = CircleY;
+		//}
+
+
+
 		SDL_RenderPresent(MyRenderer);
 	}
-
-	SDL_Delay(1000); // milliseconds 밀리세컨드 단위
 
 	SDL_DestroyRenderer(MyRenderer);
 
